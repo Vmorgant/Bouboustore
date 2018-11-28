e2011c71c5ce56dbb2eef90239d183f7951c297e
Elodie Morin <Elodie.morin1298@gmail.com>
2018-11-28 18:44:46 +0100

    Size adjust for home boxes

---
+++ js/connectBDD.js
@@ -0,0 +1,16 @@
+/*
+function connectEDT() {
+    $.post(
+        localStorage.getItem("edtConnect"),
+        
+        {salle: localStorage.getItem("room")},
+        
+        function (data) {
+            localStorage.setItem("EDT" , data);
+            checkInit();
+        },
+        
+        'text'
+    );
+}
+*/
