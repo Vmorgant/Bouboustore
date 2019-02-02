112951ebf851992b62d5ba4d3f0dc75589f590df
Victor Morgant <victor.morgant@gmail.com>
2019-02-02 11:25:59 +0100

    fixing some log error

--- home.php
+++ home.php
@@ -18,7 +18,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
-  <meta name="author" content="Dashboard">
+  <meta name="author" content="MORIN Elodie MORGANT Victor">
   <meta name="keyword" content="">
   <title>Bouboustore</title>
 
