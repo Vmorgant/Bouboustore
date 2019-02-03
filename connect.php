315e4f09a5ef69c492a814d354be731eacdd89d7
Victor Morgant <victor.morgant@gmail.com>
2019-02-03 21:57:35 +0100

    try 2

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 session_start();
 include_once 'auth.php';
 $Connect = mysqli_connect($Server, $login, $mdp, $db);
-$mysqli->set_charset("utf8");
+mysqli_set_charset("UTF8", $Connect);
 if (!$Connect)
     echo "Connexion à la base impossible";
 date_default_timezone_set('Europe/Paris');
