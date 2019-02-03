be7200b688f594de579d458f9e0d429c7a4a0593
Victor Morgant <victor.morgant@gmail.com>
2019-02-03 21:56:17 +0100

    try to define utf8

--- connect.php
+++ connect.php
@@ -2,6 +2,7 @@
 session_start();
 include_once 'auth.php';
 $Connect = mysqli_connect($Server, $login, $mdp, $db);
+$mysqli->set_charset("utf8");
 if (!$Connect)
     echo "Connexion à la base impossible";
 date_default_timezone_set('Europe/Paris');
