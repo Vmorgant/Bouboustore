01c68041d823c81d2d9fdf77fd729e9aed7add98
Victor Morgant <victor.morgant@gmail.com>
2019-02-04 10:43:20 +0100

    remove the popup +encoding ok

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 session_start();
 include_once 'auth.php';
 $Connect = mysqli_connect($Server, $login, $mdp, $db);
-mysqli_set_charset("UTF8", $Connect);
+mysqli_set_charset($Connect, "UTF8");
 if (!$Connect)
     echo "Connexion à la base impossible";
 date_default_timezone_set('Europe/Paris');
